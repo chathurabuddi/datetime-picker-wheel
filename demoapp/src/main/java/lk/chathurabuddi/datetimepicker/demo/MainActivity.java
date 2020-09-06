cb6fa701b9204a33f1953be56e403d2338057319
chathurabuddi <chathu.buddi@gmail.com>
2020-09-06 14:43:04 +0530

    demo app added

---
+++ demoapp/src/main/java/lk/chathurabuddi/datetimepicker/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package lk.chathurabuddi.datetimepicker.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
